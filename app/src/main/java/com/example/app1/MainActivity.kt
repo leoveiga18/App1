d333bffb4cc423959ee0d82fa6126a08f750070d
193985 <193985@upf.br>
2024-02-29 22:03:22 -0300

    Projeto pronto

--- app/src/main/java/com/example/app1/MainActivity.kt
+++ app/src/main/java/com/example/app1/MainActivity.kt
@@ -2,15 +2,40 @@ package com.example.app1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 var editNome:EditText? = null
 var editIdade:EditText? = null
-var textResult:TextView? = null
+var textResultado:TextView? = null
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editNome = findViewById<EditText>(R.id.edtNome)
+        editIdade = findViewById<EditText>(R.id.edtIdade)
+        textResultado = findViewById<EditText>(R.id.txtResultado)
+    }
+
+    fun calcular(view: View) {
+        val pes = Pessoa(null, editNome?.text.toString(),
+            editIdade?.text.toString().toInt())
+            textResultado?.text = pes.getDadosPessoa()
+
+
+    }
+
+    fun botaoClicar(v: View?){
+        val clicado: String = ( v as Button).text.toString()
+        when(clicado){
+            "Processar" -> println("Pressionou Processar")
+            "Limpar" -> println("Pressionou Limpar")
+        }
+
     }
 }
+
